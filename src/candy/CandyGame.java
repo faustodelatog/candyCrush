c3a88ae378d1869010f0576278cc7e7d77fcbbcd
Fausto De La Torre <faustodelatog@gmail.com>
2014-02-04 16:56:25 -0500

    ---- llenar el tablero con symbolos

--- src/candy/CandyGame.java
+++ src/candy/CandyGame.java
@@ -2,14 +2,23 @@ package candy;
 
 public class CandyGame {
 
-	private int boardDimension;
+	private String[][] board;
 
 	public CandyGame(int i) {
-		boardDimension = i;
+		board = new String[i][i];
 	}
 
 	public Object[][] getBoard() {
-		return new Object[boardDimension][boardDimension];
+		return board;
+	}
+
+	public void fill(String[] symbols) {
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				board[i][j] = symbols[0];
+			}
+		}
+
 	}
 
 }
--- src/candy/CandyGameTest.java
+++ src/candy/CandyGameTest.java
@@ -1,9 +1,17 @@
 package candy;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CandyGameTest {
+
+	private CandyGame game = null;
+
+	@Before
+	public void initTest() {
+		game = new CandyGame(9);
+	}
 
 	@Test
 	public void shouldConstructAGameWith9x9Board() {
@@ -23,6 +31,27 @@ public class CandyGameTest {
 		for (Object[] row : board) {
 			Assert.assertEquals(10, row.length);
 		}
+	}
+
+	@Test
+	public void shouldFillBoardWithSymbols() {
+		String[] symbols = { "A", "B", "C", "D" };
+		game.fill(symbols);
+		Object[][] board = game.getBoard();
+		for (Object[] row : board) {
+			for (Object e : row) {
+				Assert.assertTrue(isInArray(symbols, e));
+			}
+		}
+	}
+
+	private boolean isInArray(Object[] array, Object e) {
+		for (Object element : array) {
+			if (element.equals(e)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
