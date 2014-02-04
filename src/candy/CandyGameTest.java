5d1e3a94848b1c222b85ef99d1fa352ec6cada55
Fausto De La Torre <faustodelatog@gmail.com>
2014-02-04 16:30:34 -0500

    primera prueba de construccion del juego

---
+++ src/candy/CandyGameTest.java
@@ -0,0 +1,17 @@
+package candy;
+
+import org.junit.Test;
+
+public class CandyGameTest {
+
+	@Test
+	public void shouldConstructAGameWith9x9Board() {
+		CandyGame game = new CandyGame(9);
+		Object[][] board = game.getBoard();
+		Assert.assertEquals(9, board.length);
+		for (Object[] row : board) {
+			Assert.assertEquals(9, row.length);
+		}
+	}
+
+}
