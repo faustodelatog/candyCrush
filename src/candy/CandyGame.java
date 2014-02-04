3834f54dde1e233926e41527fe3b67909e156045
Fausto De La Torre <faustodelatog@gmail.com>
2014-02-04 16:35:46 -0500

    Pasa la prueba en verde

---
+++ src/candy/CandyGame.java
@@ -0,0 +1,13 @@
+package candy;
+
+public class CandyGame {
+
+	public CandyGame(int i) {
+
+	}
+
+	public Object[][] getBoard() {
+		return new Object[9][9];
+	}
+
+}
--- src/candy/CandyGameTest.java
+++ src/candy/CandyGameTest.java
@@ -1,5 +1,6 @@
 package candy;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CandyGameTest {
